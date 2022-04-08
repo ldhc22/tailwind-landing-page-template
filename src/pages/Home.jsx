610040d9587e132ac8b863891d4632b84db90c8d
lhernandezc <lhernandezc@actinver.com.mx>
2022-04-07 19:38:55 -0500

    Initial commit with photo section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,35 +1,33 @@
-import React from 'react';
+import React from "react";
 
-import Header from '../partials/Header';
-import HeroHome from '../partials/HeroHome';
-import FeaturesHome from '../partials/Features';
-import FeaturesBlocks from '../partials/FeaturesBlocks';
-import Testimonials from '../partials/Testimonials';
-import Newsletter from '../partials/Newsletter';
-import Footer from '../partials/Footer';
+import Header from "../partials/Header";
+import HeroHome from "../partials/HeroHome";
+import PhotoSection from "../partials/PhotoSection";
+import FeaturesHome from "../partials/Features";
+import FeaturesBlocks from "../partials/FeaturesBlocks";
+import Testimonials from "../partials/Testimonials";
+import Newsletter from "../partials/Newsletter";
+import Footer from "../partials/Footer";
 
 function Home() {
   return (
-    <div className="flex flex-col min-h-screen overflow-hidden">
-
+    <div className="flex flex-col min-h-screen overflow-hidden bg-slate-400">
       {/*  Site header */}
       <Header />
 
       {/*  Page content */}
       <main className="flex-grow">
-
         {/*  Page sections */}
         <HeroHome />
+        <PhotoSection />
         <FeaturesHome />
         <FeaturesBlocks />
         <Testimonials />
         <Newsletter />
-
       </main>
 
       {/*  Site footer */}
       <Footer />
-
     </div>
   );
 }
---
+++ src/partials/PhotoSection.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import DanseImage from "../images/danse-placeholder.jpeg";
+
+function PhotoSection() {
+  return (
+    <section className="relative">
+      <div className="md:grid md:grid-cols-12 max-w-xl md:max-w-none md:w-full bg-slate-200">
+        <div className="flex-col md:col-span-8 md:h-screen md:transform gradient-mask-b-0 md:gradient-mask-r-0 overflow-y-clip">
+          <img
+            className="relative object-cover h-80 md:h-screen md:transform md:scale-110"
+            src={DanseImage}
+            alt="DanSe"
+          />
+        </div>
+        <div className="flex-col flex justify-center md:col-span-4 bg-slate-200 md:transform md:-translate-x-12 md:-skew-x-12 h-80 md:h-screen gradient-mask-t-20 md:gradient-mask-l-20">
+          <div className="w-3/4 md:transform md:skew-x-12 md:translate-x-6 mx-auto ">
+            <h1 className="text-center text-xl md:text-2xl  ">
+              No será perfecto. Pero, ¿tiene que serlo?
+            </h1>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default PhotoSection;
